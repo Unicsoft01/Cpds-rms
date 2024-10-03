d1e1abf9330fd8b8e831089c0eb22cd5ed34c899
Muhammed Yakub <yakubmuhammed51@gmail.com>
2024-10-03 21:20:06 +0100

    initial commit, published config file and added default template asset files in public

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\HtmlMinifier;
+// use App\Livewire\Tasks\Index;
+
+Route::middleware([HtmlMinifier::class])->group(function () {
+
+    Route::view('/', 'welcome')->name('welcome');
+
+    Route::middleware(['auth', 'verified'])->group(function () {
+
+        Route::view('dashboard', 'dashboard')->name('dashboard');
+
+        Route::view('profile', 'profile')->name('profile');
+
+        // Route::get('/tasks/index', Index::class)->name('tasks.index');
+    });
+
+
+    require __DIR__ . '/auth.php';
+});
